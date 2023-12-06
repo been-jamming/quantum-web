eaf3abccfc03e3662badd9319302953f41468d56
been-jamming <computergeek3.142@gmail.com>
2023-12-05 23:47:04 -0700

    beginning implicit qr algorithm

--- diagonalize.js
+++ diagonalize.js
@@ -18,6 +18,28 @@ function norm_squared(vector){
 	return sum;
 }
 
+function givens_rotate(vector, c, s, x, y){
+	var x_1 = vector[x]*c - vector[y]*s;
+	var y_1 = vector[x]*s + vector[y]*c;
+
+	vector[x] = x_1;
+	vector[y] = y_1;
+
+	return vector;
+}
+
+
+function givens_rotation(n, c, s, x, y){
+	var output = math.identity(n).toArray();
+
+	output[x][x] = c;
+	output[y][y] = c;
+	output[x][y] = -s;
+	output[y][x] = s;
+
+	return output;
+}
+
 function hessenberg(matrix){
 	if(matrix.length <= 2){
 		return {unitary: math.identity(matrix.length), result: matrix};
@@ -51,4 +73,37 @@ function hessenberg(matrix){
 	var next_matrix = math.subset(next_matrix, math.index(math.range(1, matrix.length), math.range(1, matrix.length)), next_result.result);
 
 	return {unitary: math.multiply(U, new_U), result: next_matrix};
+}
+
+function tridiag_to_matrix(diag, subdiag){
+	var output = math.identity(diag.length).toArray();
+
+	for(var i = 0; i < diag.length - 1; i++){
+		output[i][i] = diag[i];
+		output[i + 1][i] = subdiag[i];
+		output[i][i + 1] = math.conj(subdiag[i]);
+	}
+
+	output[diag.length - 1][diag.length - 1] = diag[diag.length - 1];
+
+	return output;
+}
+
+//Not correct
+//I must *conjugate* by the givens rotation below in order to apply the implicit q theorem
+function implicit_qr_step(diag, subdiag){
+	var x = diag[0];
+	var y = subdiag[0];
+
+	var d = math.sqrt(x*x + y*y);
+	var c = x/d;
+	var s = -y/d;
+
+	diag[0] = d;
+	subdiag[0] = 0;
+	diag[1] = y*s + c*diag[1];
+	var bulge = -s*subdiag[1];
+	subdiag[1] *= c;
+
+	return {diag: diag, subdiag: subdiag, bulge: bulge};
 }
